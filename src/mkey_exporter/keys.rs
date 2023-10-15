0e2b2620bee9df9a0b01f02021f0f7f3891d9c9c
Nick Pillitteri <nick@56quarters.xyz>
2023-10-15 11:39:49 -0400

    Unit tests for key parsing logic

--- src/mkey_exporter/keys.rs
+++ src/mkey_exporter/keys.rs
@@ -36,3 +36,130 @@ impl<'a> LabelParser<'a> {
         labels
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::LabelParser;
+    use crate::config::{Rule, RuleGroup, RulePattern};
+    use mtop_client::Meta;
+
+    fn new_meta(key: &str) -> Meta {
+        Meta {
+            key: key.to_owned(),
+            ..Default::default()
+        }
+    }
+
+    fn user_rule() -> Rule {
+        Rule {
+            pattern: RulePattern::new(r"\w+:([\w-]+):").unwrap(),
+            label_name: "user".to_owned(),
+            label_value: "u$1".to_owned(),
+        }
+    }
+
+    fn type_rule() -> Rule {
+        Rule {
+            pattern: RulePattern::new(r"([\w-]+):\w+:").unwrap(),
+            label_name: "type".to_owned(),
+            label_value: "$1".to_owned(),
+        }
+    }
+
+    fn specific_type_rules() -> Vec<Rule> {
+        vec![
+            Rule {
+                pattern: RulePattern::new(r"u-c:\w+:").unwrap(),
+                label_name: "type".to_owned(),
+                label_value: "cart".to_owned(),
+            },
+            Rule {
+                pattern: RulePattern::new(r"u-p:\w+:").unwrap(),
+                label_name: "type".to_owned(),
+                label_value: "profile".to_owned(),
+            },
+            Rule {
+                pattern: RulePattern::new(r"([\w-]+):\w+:").unwrap(),
+                label_name: "type".to_owned(),
+                label_value: "unknown".to_owned(),
+            },
+        ]
+    }
+
+    #[test]
+    fn test_extract_single_label() {
+        let meta = new_meta("u-p:12345:something");
+        let group = RuleGroup {
+            name: "test".to_owned(),
+            rules: vec![user_rule()],
+        };
+
+        let parser = LabelParser::new(&group);
+        let labels = parser.extract(&meta);
+
+        assert_eq!(vec![("user".to_owned(), "u12345".to_owned())], labels);
+    }
+
+    #[test]
+    fn test_extract_multiple_labels() {
+        let meta = new_meta("u-p:12345:something");
+        let group = RuleGroup {
+            name: "test".to_owned(),
+            rules: vec![user_rule(), type_rule()],
+        };
+
+        let parser = LabelParser::new(&group);
+        let labels = parser.extract(&meta);
+
+        assert_eq!(
+            vec![
+                ("user".to_owned(), "u12345".to_owned()),
+                ("type".to_owned(), "u-p".to_owned()),
+            ],
+            labels
+        );
+    }
+
+    #[test]
+    fn test_extract_multiple_labels_precedence() {
+        let meta1 = new_meta("u-p:123:something");
+        let meta2 = new_meta("u-c:456:something");
+        let meta3 = new_meta("u-v:789:something");
+
+        let mut rules = Vec::new();
+        rules.push(user_rule());
+        rules.extend(specific_type_rules());
+
+        let group = RuleGroup {
+            name: "test".to_owned(),
+            rules,
+        };
+
+        let parser = LabelParser::new(&group);
+        let labels1 = parser.extract(&meta1);
+        let labels2 = parser.extract(&meta2);
+        let labels3 = parser.extract(&meta3);
+
+        assert_eq!(
+            vec![
+                ("user".to_owned(), "u123".to_owned()),
+                ("type".to_owned(), "profile".to_owned()),
+            ],
+            labels1
+        );
+        assert_eq!(
+            vec![
+                ("user".to_owned(), "u456".to_owned()),
+                ("type".to_owned(), "cart".to_owned()),
+            ],
+            labels2
+        );
+        assert_eq!(
+            vec![
+                ("user".to_owned(), "u789".to_owned()),
+                ("type".to_owned(), "unknown".to_owned()),
+            ],
+            labels3
+        );
+    }
+}
